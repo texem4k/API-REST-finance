6b72d58126d131b23874e05677728e5dc0c2d97d
texem4k <texenerybordon123@gmail.com>
2026-07-30 16:09:24 +0100

    Avance del front-end previo a la implementación de Angular

---
+++ src/main/java/com/apifinance/apirestfinance/control/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.apifinance.apirestfinance.control;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Permite todas las peticiones sin login
+                )
+                .csrf(AbstractHttpConfigurer::disable); // Opcional: Deshabilita CSRF si estás creando APIs
+
+        return http.build();
+    }
+}
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,5 +5,4 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 async function init(){
     const users = await (await fetch("http://localhost:8080/usuarios/userlist")).json();
-
 }
